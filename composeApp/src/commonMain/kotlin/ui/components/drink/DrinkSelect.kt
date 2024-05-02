d07dbd0e1cd6979bfad146da6b82e51fe41231c3
Ben Burwood <ben.burwood@outlook.com>
2024-05-02 17:51:36 +0100

    Add DrinkSelect Box

---
+++ composeApp/src/commonMain/kotlin/ui/components/drink/DrinkSelect.kt
@@ -0,0 +1,38 @@
+package ui.components.drink
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Button
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import data.drink.Drink
+
+
+@Composable
+fun DrinkSelect(drinks: List<Drink>, onDrinkSelected: (Drink) -> Unit) {
+    var expanded: Boolean by remember { mutableStateOf(false) }
+    var selectedValue: Drink? by remember { mutableStateOf(null) }
+
+    Box {
+        Button(onClick = { expanded = true }) {
+            Text(selectedValue?.name ?: "Select")
+        }
+        DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }, scrollState = rememberScrollState()) {
+            drinks.forEach {
+                DropdownMenuItem(onClick = {
+                    expanded = false
+                    selectedValue = it
+                    onDrinkSelected(it)
+                }) {
+                    Text(it.name)
+                }
+            }
+        }
+    }
+}
